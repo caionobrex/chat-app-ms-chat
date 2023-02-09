fd681862e1ce6f7affe3c57a9358c26651a52839
caionobrex <marialoucos@hotmail.com.br>
2023-02-09 13:01:33 -0300

    set pusher credentials as env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,14 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatController } from './controller/chat.controller';
 import { ChatService } from './service/chat.service';
 import * as Pusher from 'pusher';
 import { PrismaService } from './prisma.service';
 
 const pusher = new Pusher({
-  appId: '1513456',
-  key: '17c198678267f49145cc',
-  secret: '1a4fbbae3844669283a5',
-  cluster: 'mt1',
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_KEY,
+  secret: process.env.PUSHER_SECRET,
+  cluster: process.env.PUSHER_CLUSTER,
   useTLS: true,
 });
 
